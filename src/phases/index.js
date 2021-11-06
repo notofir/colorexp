0d90e050792689de3d48ad0a780e94b47da3ff15
notofir <ofirarias@gmail.com>
2021-11-06 14:16:40 +0200

    Uniting hint file and phases

--- src/phases/index.js
+++ src/phases/index.js
@@ -1,4 +1,14 @@
-import createHint from "../hint";
+function createHint({
+  groups = [{ size: 0, certainty: 0 }],
+  delay = 0,
+  autoHintClicks = { min: -1, max: -1 },
+}) {
+  return {
+    groups: groups,
+    delay: delay,
+    autoHintClicks: autoHintClicks,
+  };
+}
 
 function createPhase({
   taskName = "ColorsTrial",
